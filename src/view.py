55cc6391152964a9d25c7cc1a5da181fd9564b2d
Elias Seidl <oelyaz@fedora.fritz.box>
2025-03-05 18:09:16 +0100

    blocked mouse interaction with entries and block tab key

--- src/view.py
+++ src/view.py
@@ -24,10 +24,14 @@ class View:
             for row in range(5):
                 self.entry = tk.Entry(self.grid_frame, width=2,  justify="center",
                                       font=("Terminal", 30), bg="#1d2021", fg="#ebdbb2")
-                self.entry.grid(column=col, row=row, ipadx=4, ipady=4, padx=0, pady=10)
+                self.entry.bind("<Key>", lambda e: "break")  # Blocks keyboard input
+                self.entry.bind("<Button-1>", lambda e: "break") # Blocks Mouse clicks
+                self.entry.grid(column=col, row=row, ipadx=4, ipady=0, padx=2, pady=12)
                 entry_row.append(self.entry)
             self.entries.append(entry_row)
         self.grid_frame.place(relx=0.5, rely=0.6, anchor="center")
+        self.entries[0][0].focus_set()
+        self.entries[0][0].unbind("<Key>")  # Allow keyboard input for this entry
 
         self.label = tk.Label(self.frame, bg="#282828", fg="#d3869b", text="W6rdle", font=("Terminal", 70))
         self.label.place(relx=0.5, rely=0.15, anchor="center")
